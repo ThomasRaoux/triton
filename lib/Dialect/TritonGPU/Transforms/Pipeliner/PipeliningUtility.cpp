b0267381e32648f5ff0108c4979af46f51a405b9
Thomas Raoux <thomas.raoux@openai.com>
2024-03-03 21:59:13 -0800

    fix lit test

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/PipeliningUtility.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/PipeliningUtility.cpp
@@ -36,6 +36,8 @@ Operation *mlir::triton::predicateOp(RewriterBase &rewriter, Operation *op,
     return op;
   if (isa<ttg::AsyncWaitOp>(op))
     return op;
+  if (isa<ttg::SharedLoad>(op))
+    return op;
   if (auto asyncCopyOp = dyn_cast<ttg::AsyncSharedCopy>(op)) {
     rewriter.setInsertionPoint(asyncCopyOp);
     Value mask = getPredMask(rewriter, asyncCopyOp.getSrc().getType(),
