9211d5cdc89f494eebd74e8db0a953a6274871b4
Thomas Raoux <thomas.raoux@openai.com>
2025-04-14 02:36:22 +0200

    [WIP] Support memdesc_reshape to allow efficient HBM layout for mmav3/mmav5
    Adds an op to reshape memdesc types. This allows to support cases
    where we want to reshape a value loaded from global to shared without
    going through register.

--- include/triton/Dialect/TritonGPU/IR/Dialect.h
+++ include/triton/Dialect/TritonGPU/IR/Dialect.h
@@ -278,6 +278,11 @@ bool areLayoutsEquivalent(ArrayRef<int64_t> shape, Attribute lhs,
 
 // Return true if the innermost numElems are contiguous.
 bool isInnermostContiguous(MemDescType type, unsigned numElems);
+
+
+LinearLayout inferReshapeLinearLayout(ArrayRef<int64_t> srcShape,
+                                      Attribute srcEnc,
+                                      ArrayRef<int64_t> dstShape);
 } // namespace mlir::triton::gpu
 
 #endif // TRITON_DIALECT_TRITONGPU_IR_DIALECT_H_
